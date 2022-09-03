2c41ac1075bc1af83755a3d0f73dc5f1192bcec4
Aviad Hahami <aviadhahami@gmail.com>
2022-09-03 14:37:46 +0300

    fixup

--- start.sh
+++ start.sh
@@ -2,23 +2,11 @@
 
 target="$1"
 
-# Add /FUZZ if it is missing in the target
-echo "Target URL: $target"
-if [[ $target != *"FUZZ"* ]]; then
-    echo $target | grep '/$'
-    if [ $? -eq 1 ]; then
-        target="${target}/"
-    fi
-    target="${target}FUZZ"
-    echo "Changed target URL to: $target"
-fi
-
 set -x
 
 # Scan
-ffuf -w words_and_files_top5000.txt \
+ffuf -w ./wordlists/words_and_files_top20000.txt \
     -u $target \
-    -H "X-Scanner: FFUF" \
     -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0" \
     -of all \
     -o /var/reports/ffuf_scan \
